a47ccce26b3f317b690f8ea807baecaf98128159
Atharv Powalkar <87745693+07-Atharv@users.noreply.github.com>
2023-04-24 19:03:43 +0530

    Add files via upload

---
+++ wdow.js
@@ -0,0 +1,33 @@
+// console.log("These we will create window object");
+// // DOM  = Document Object Model The doc object is part if window object
+// // window is client side object and it is globally object
+// let a = window.document ;
+// console.log(a);
+// // window.alert("These is alert window");
+// // let p = window.prompt("Enter your name and check the console");
+// // console.log(p);
+// // let c = window.confirm("Are you sure");
+// // console.log(c);
+// let iw = innerWidth;
+// let ih = innerHeight;
+// console.log("Innwe width = "+iw +"Inner Height = "+ ih);
+// let it = scrollX ;
+// let ip = scrollY ;
+// console.log(it);
+// console.log(ip);
+// f = location;
+// //location.href 
+// //location.reload
+// //location.toString
+// //window.history
+// //history.go{-1}
+// //history.length
+// console.log(f)
+// let a = window.prompt("Heyyy");
+// alert("Dangerous");
+// confirm("Sure");
+
+
+// element node = head (which is the element)
+// text node = content of the node
+// attributte node = div id = id is the attribute
